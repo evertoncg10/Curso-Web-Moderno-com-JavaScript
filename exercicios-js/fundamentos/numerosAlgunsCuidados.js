b240154d8d9058c420f8ef34eb141c7842765dfd
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2019-04-13 15:43:06 -0300

    Aula 17. Number: Alguns Cuidados

---
+++ exercicios-js/fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,7 @@
+console.log(7 / 0);
+console.log("10" / 2); 
+console.log("a" / 2); 
+console.log("show" * 2); // NaN
+console.log(0.1 + 0.7); // imprecisão
+//console.log(10.toString());
+console.log((10.345).toFixed(2));

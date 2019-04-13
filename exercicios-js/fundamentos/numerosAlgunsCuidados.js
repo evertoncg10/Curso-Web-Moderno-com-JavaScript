58caa5acaacd2da7d42d6eb1a3d52a65c4212949
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2019-04-13 16:56:48 -0300

    Aula 19. Tipos em JavaScript: String

--- exercicios-js/fundamentos/numerosAlgunsCuidados.js
+++ exercicios-js/fundamentos/numerosAlgunsCuidados.js
@@ -1,5 +1,7 @@
 console.log(7 / 0);
 console.log("10" / 2); 
+console.log("3" + 2); // A string tem preferência 
+console.log("3" - 2); 
 console.log("a" / 2); 
 console.log("show" * 2); // NaN
 console.log(0.1 + 0.7); // imprecisão

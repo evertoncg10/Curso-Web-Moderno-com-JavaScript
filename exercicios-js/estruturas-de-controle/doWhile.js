f2c35accb4df674517c58d2be8d81f008fdd3910
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2019-05-01 19:06:50 -0300

    Aula 58. Usando a Estrutura DO/WHILE

---
+++ exercicios-js/estruturas-de-controle/doWhile.js
@@ -0,0 +1,15 @@
+function getInteiroAleatorioEntre(min, max) {
+    const valor = Math.random() * (max - min) + min;
+    return Math.floor(valor);
+
+}
+
+let opcao = -1;
+let i = -1;
+
+do {
+    opcao = getInteiroAleatorioEntre(-1, 10);
+    console.log(`Opção escolhida foi ${opcao}.`);
+} while(i != -1) 
+
+console.log('Até a próxima');

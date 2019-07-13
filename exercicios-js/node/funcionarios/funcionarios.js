1f8e3b52931be6dfbd24d06e122c7926a224903e
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2019-07-13 15:12:35 -0300

    Aula - Node: Resolvendo Desafio

--- exercicios-js/node/funcionarios/funcionarios.js
+++ exercicios-js/node/funcionarios/funcionarios.js
@@ -3,5 +3,14 @@ const axios = require('axios');
 
 axios.get(url).then(response => {
     const funcionarios = response.data;
-    console.log(funcionarios);
+    //console.log(funcionarios);
+    
+    // buscar no array de funcionarios a mulher chinesa com menor sálario
+    const isMulher = funcionario => funcionario.genero === 'F';
+    const isChineses = funcionario => funcionario.pais === 'China';
+    const menorSalario = (funcionario, funcionarioAtual) => {
+        return funcionario.salario < funcionarioAtual.salario ? funcionario : funcionarioAtual;
+    };
+    const func = funcionarios.filter(isMulher).filter(isChineses).reduce(menorSalario);
+    console.log(func);
 });
